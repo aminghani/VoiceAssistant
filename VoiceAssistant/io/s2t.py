7bbc4324345048c0fed225b8f1e8b2838bf17241
aminghani <aminkhosroshahi80328@gmail.com>
2024-07-27 17:24:32 +0200

    rasa progress

--- VoiceAssistant/io/s2t.py
+++ VoiceAssistant/io/s2t.py
@@ -1,5 +1,6 @@
 from RealtimeSTT import AudioToTextRecorder
 from VoiceAssistant.api.bot_api import send_message
+from VoiceAssistant.io.t2s import play
 
 recorder = AudioToTextRecorder(model='small')
 
@@ -7,6 +8,9 @@ def process():
     def process_text(text):
         res = send_message(text)
         print(f"User: {text}")
+        print(res)
+        print('========================')
+        play(res[0])
         for response in res:
             print(f"Bot: {response}")
         
--- VoiceAssistant/io/t2s.py
+++ VoiceAssistant/io/t2s.py
@@ -2,22 +2,16 @@ import numpy as np
 import sounddevice as sd
 from TTS.api import TTS
 
-#en: tts_models/en/ljspeech/vits, fa: tts_models/fa/custom/glow-tts
 tts = TTS("tts_models/en/ljspeech/vits")
 
-# List available 🐸TTS models
-#print(dir(TTS().list_models()))
+
 print(TTS().list_models().list_models())
 print(TTS().list_models().list_langs())
-# Text to be converted to speech
-text = "hello, this is a test."
-#print(tts.speakers)
-# Generate the speech
-#print(tts.voice_converter.vc_config.audio.output_sample_rate)
+
 
 
 def play(txt: str):
-    wav = tts.tts(text)
+    wav = tts.tts(txt)
 
     wav = np.array(wav, dtype=np.float32)
 
